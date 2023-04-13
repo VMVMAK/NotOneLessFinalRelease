3725f17ac14ec116b6b5ab49e954f80da39f9831
VickyMAK <vickymak@gmail.com>
2023-04-13 11:26:02 -0500

    My Tournament, Torunament Details, Login UI update

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports =
 {
     // localhost URI to connect local DB
-    "URI":"mongodb://localhost/project229",    
+    "URI":"mongodb://localhost/project229",
     // remote URI to connect remote DB
-    "RemoteURI":process.env.RemoteURI  
+    "RemoteURI":process.env.RemoteURI
 }
